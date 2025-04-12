a99761a6718983899367249270098bb81a09eb8b
Krystian Czaplicki <krystianczaplicki@outlook.com>
2025-04-12 09:28:34 +0200

    feat: initialize project with hexagonal architecture modules

---
+++ src/server/bootstrap/src/main/java/org/kczaplicki/employeeinsight/bootstrap/EmployeeInsightApplication.java
@@ -0,0 +1,13 @@
+package org.kczaplicki.employeeinsight.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeInsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeInsightApplication.class, args);
+	}
+
+}
